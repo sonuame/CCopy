74f0fd5c030afd2507633bf5eeb3bb00c50fa1a9
Sunil Kumar <sonuame@aol.com>
2020-05-28 19:44:46 +0530

    version 1.0 stabalized

--- CCopy/Program.cs
+++ CCopy/Program.cs
@@ -4,11 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CCopy
 {
     class Program
     {
+        static bool Silent;
 
         static void Main(string[] args)
         {
@@ -26,12 +28,23 @@ namespace CCopy
                 "-r|--replace",
                 "Set the Over-Write mode if destination file exists", CommandOptionType.NoValue);
 
+            var subDir = app.Option(
+                "-a|--all",
+                "Select Sub directories too", CommandOptionType.NoValue);
+
             var fileTypes = app.Option(
                 "-t|--types <types>",
                 "Source file types e.g *.jpg *.png", CommandOptionType.MultipleValue);
 
+            var silent = app.Option(
+                "-q|--quiet",
+                "No Progress display", CommandOptionType.NoValue);
+
             app.OnExecute(() =>
             {
+                Console.WriteLine();
+                Console.WriteLine("CCopy version 1.0");
+
                 if (!pathFrom.HasValue())
                 {
                     Console.WriteLine("--source/-s is required");
@@ -39,19 +52,33 @@ namespace CCopy
                 }
 
 
-                var from = pathFrom.Value();
-                var to = pathTo.HasValue() ? pathTo.Value() : AppDomain.CurrentDomain.BaseDirectory;
+                var from = Path.Combine(Directory.GetCurrentDirectory(), pathFrom.Value());
+                var to = pathTo.HasValue() ? pathTo.Value() : Directory.GetCurrentDirectory();
                 var replace = overWrite.HasValue();
-                var types = fileTypes.HasValue() ? fileTypes.Values.ToArray() : new string[] { "*.*" };
+                var resursive = subDir.HasValue();
+                Silent = silent.HasValue();
 
-                Copy(from, to, "", replace, types);
+                var types = fileTypes.HasValue() ?
+                    (fileTypes.Value().Contains(",") ? fileTypes.Value().Split(",") :
+                    fileTypes.Value().Split(" ")) : new string[] { "*.*" };
+
+
+                Copy(
+                    from: from,
+                    to: to,
+                    rootDir: "",
+                    recursive: resursive,
+                    overwrite: replace,
+                    types: types
+                );
                 return 1;
             });
 
             app.Execute(args);
+
         }
 
-        static void Copy(string from, string to, string rootDir = "", bool overwrite = true, params string[] types)
+        static void Copy(string from, string to, string rootDir = "", bool recursive = false, bool overwrite = true, params string[] types)
         {
             var isdirFrom = from.GetDIRInfo().Attributes == FileAttributes.Directory;
             var isdirTo = to.GetDIRInfo().Attributes == FileAttributes.Directory || to.GetDIRInfo().Attributes == (FileAttributes)(-1);
@@ -66,20 +93,21 @@ namespace CCopy
 
                 List<FileInfo> files = new List<FileInfo>();
                 if (types?.Length > 0)
-                    types.ToList().ForEach(type => files.AddRange(from.GetDIRInfo().GetFiles(type, SearchOption.AllDirectories)));
+                    types.ToList().ForEach(type => files.AddRange(from.GetDIRInfo().GetFiles(type, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)));
                 else
-                    files.AddRange(from.GetDIRInfo().GetFiles("*.*", SearchOption.AllDirectories));
+                    files.AddRange(from.GetDIRInfo().GetFiles("*.*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+                Console.WriteLine($"{files.Count} total files");
 
-                using (var pbar = new ProgressBar(files.Count, "progress bar is on the bottom now", options))
+                var count = 0;
+                Parallel.ForEach(files, file =>
                 {
-                    var count = 0;
-                    files.ForEach(file =>
+                    Copy(file.FullName, to, from, overwrite);
+                    if (!Silent)
                     {
-                        Copy(file.FullName, to, from, overwrite);
                         ++count;
-                        pbar.Tick($"File {count} of {files.Count} copied");
-                    });
-                }
+                        Console.Write($"..{count}");
+                    }
+                });
             }
 
             if (!isdirFrom && isdirTo)
@@ -98,6 +126,7 @@ namespace CCopy
                 {
                     Directory.CreateDirectory(to.GetFileInfo().DirectoryName);
                     File.Copy(from, to, overwrite);
+                    Console.WriteLine("1 File(s) copied");
                 }
             }
         }
